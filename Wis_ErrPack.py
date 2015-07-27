ed81c3ef39331bcc8be323ce823b5d817a4b4b40
Emily Zhang <ezmay15@gmail.com>
2015-07-27 14:18:02 -0400

    Added returns to functions in ErrPack

--- Wis_ErrPack.py
+++ Wis_ErrPack.py
@@ -5,11 +5,18 @@
 #Scott Prahl, who had zipped a version of Wiscombe's code and made it
 #downloadable (http://omlc.org/software/mie/)
 
+#Definitions/specifications for subroutines in the original Fortran file
+#(subroutines -> functions in this Python code) were kept in this code as comments.
+#If output is formatted oddly, the first place to look in this code would be the print
+#statements, since those have been converted in the simplest way possible and may have lost
+#some specifications possible with Fortran's "write" statement.
+
+
 def ErrMsg( MESSAG, FATAL ):
     
     """
-    Print out a warning or error message; abort if error 
-    after making symbolic dump (machine-specific)
+        Print out a warning or error message; abort if error 
+        after making symbolic dump (machine-specific)
 
     .. Scalar Arguments ..
 
@@ -33,34 +40,38 @@ def ErrMsg( MESSAG, FATAL ):
     MAXMSG = 100 
     MSGLIM = False
 
+        
     if FATAL:
         print '******ERROR***** ', MESSAG
         
-    #Example symbolic dump call for Cray
-    #CALL SYMDUMP( '-B -c3' )
+        #Example symbolic dump call for Cray
+        #CALL SYMDUMP( '-B -c3' )
+
+        import sys
+        sys.exit()
+
     
     NUMMSG = NUMMSG + 1
-    
-    #while True loop allows MSGLIM condition to end the function with a break
-    while True:
-        if MSGLIM: 
-            break
-    
-        if NUMMSG <= MAXMSG:
-            print '******WARNING***** ', MESSAG
-            break
         
-        else:
-            print '****** TOO MANY WARNING MESSAGES -- They will no longer be printed *******'
-            MSGLIM = True
-            break
+    if MSGLIM: 
+        return
+    
+    if NUMMSG <= MAXMSG:
+        print '******WARNING***** ', MESSAG
+        
+    else:
+        print '****** TOO MANY WARNING MESSAGES -- They will no longer be printed *******'
+        MSGLIM = True
+            
+    return
 
 
 
 def WrtBad( VarNam ):
     
     """
-    Write names of erroneous variables and return 'TRUE'
+        Write names of erroneous variables and return 'TRUE'
+        
     INPUT : VarNam = Name of erroneous variable to be written (CHARACTER, any length)
 
     .. Scalar Arguments ..
@@ -82,23 +93,25 @@ def WrtBad( VarNam ):
     NUMMSG = 0 
     MAXMSG = 50 
 
+
     NUMMSG = NUMMSG + 1
     print '**** Input variable ', VarNam, ' in error  ****'
-    return True
-
+    
     if NUMMSG == MAXMSG :
         ErrMsg('Too many input errors. Aborting...', True)
+
+    return True
 
 
 
 def WrtDim( DimNam, Minval ): 
     
     """
-    Write name of too-small symbolic dimension and 
-    the value it should be increased to; return 'TRUE'
+        Write name of too-small symbolic dimension and 
+        the value it should be increased to; return 'TRUE'
 
     INPUT: DimNam = string; Name of symbolic dimension which is too small (CHARACTER, any length)
-            Minval = int; Value to which that dimension should be increased (at least)
+           Minval = int; Value to which that dimension should be increased (at least)
 
     .. Scalar Arguments ..
 
@@ -108,6 +121,7 @@ def WrtDim( DimNam, Minval ):
     """
     
     print '**** Symbolic dimension ', DimNam, ' should be increased to at least ', Minval
+    
     return True
 
 
@@ -124,8 +138,9 @@ def TstBad( VarNam, RelErr ):
     REAL      RelErr
     ..
     """
-    
-    print '*** Output variable ', VarNam, ' differed by ', RelErr, 
-    ' percent from correct value.  Self-test failed.'
+
+    print '*** Output variable ', VarNam, ' differed by ', 100. * RelErr, \
+          ' percent from correct value.  Self-test failed.'
+          
     return False
 
